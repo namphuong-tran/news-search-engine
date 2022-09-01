4984bf45f4b7f552b88d9358701476e0736de802
phuongtrannam <trannamphuonga197@gmail.com>
2022-09-01 22:27:46 +0900

    sql statement to create table

---
+++ mysql-client/init_table.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS es_table;
+CREATE TABLE es_table (
+  id CHAR(36) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY unique_id (id),
+  title TEXT NOT NULL,
+  newspaper VARCHAR(50) NOT NULL,
+  authors VARCHAR(200),
+  publish_date DATETIME, 
+  keywords TEXT,
+  summary TEXT, 
+  text TEXT,
+  url VARCHAR(200) NOT NULL,
+  modification_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
