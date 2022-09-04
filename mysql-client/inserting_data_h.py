66272ea36622233c7dfa5c5f376c42859cdfee99
phuongtrannam <trannamphuonga197@gmail.com>
2022-09-04 23:30:29 +0900

    fix create query bug

--- mysql-client/inserting_data_h.py
+++ mysql-client/inserting_data_h.py
@@ -4,6 +4,7 @@ import pymysql
 import pandas as pd
 from datetime import datetime
 
+
 class ReadConfig:
     def __init__(self):
         self.base_dir = os.path.dirname(__file__)
@@ -12,16 +13,18 @@ class ReadConfig:
         file_path = os.path.abspath(os.path.join(self.base_dir, 'config.cfg'))
         cfg = configparser.RawConfigParser()
         cfg.read(file_path)
-        
+
         dic = {}
-        #mysql configuration
+        # mysql configuration
         mysql = 'LOCAL_DATABASE'
         mysql_host = cfg.get(mysql, 'host')
         mysql_user = cfg.get(mysql, 'user')
         mysql_password = cfg.get(mysql, 'password')
         mysql_database = cfg.get(mysql, 'database')
-        dic.update({'mysql': {'host': mysql_host, 'user': mysql_user, 'password': mysql_password, 'database': mysql_database}})
+        dic.update({'mysql': {'host': mysql_host, 'user': mysql_user,
+                   'password': mysql_password, 'database': mysql_database}})
         return dic
+
 
 class MySql:
     def __init__(self, cfg_dic, cfg_name):
@@ -34,7 +37,8 @@ class MySql:
         self.conn, self.cur = self.connect()
 
     def connect(self):
-        conn = pymysql.connect(host=self.host, user=self.user, password=self.password, database=self.database, charset="utf8mb4", use_unicode=True)
+        conn = pymysql.connect(host=self.host, user=self.user, password=self.password,
+                               database=self.database, charset="utf8mb4", use_unicode=True)
         cur = conn.cursor()
         return conn, cur
 
@@ -47,23 +51,32 @@ class MySql:
             self.conn.commit()
 
     @staticmethod
-    def create_insert_sql(table, insert_type, field_len, fields):
-        sql = """{} INTO {} ({}) VALUES ({})""".format(insert_type, table, ','.join(fields), ','.join(field_len * ['%s']))
+    def create_insert_sql(table, insert_type, field_len, fields=None):
+        if isinstance(fields, list):
+            sql = """{} INTO {} ({}) VALUES ({})""".format(insert_type, table, ','.join(fields),
+                                                           ','.join(field_len * ['%s']))
+        else:
+            sql = """{} INTO {} VALUES ({})""".format(
+                insert_type, table, ','.join(field_len * ['%s']))
+        print(sql)
         return sql
 
-if __name__ == '__main__':
-    file_name = 'D:\dthcsm\\2022_sw_pro\\news-search-engine\mysql-client\data\cnn_2021_test.csv'
-    df = pd.read_csv(file_name)
-    fields = list(df.columns)
 
+if __name__ == '__main__':
+    file_name = '/home/namphuong/Downloads/cnn_2021_test.csv'
+    df = pd.read_csv(file_name)
     cfg = ReadConfig()
     cfg_dic = cfg.get_config()
     mysql = MySql(cfg_dic, 'mysql')
-    user_sql = mysql.create_insert_sql('es_table', 'REPLACE', len(fields), fields)
+    # user_sql = mysql.create_insert_sql('es_table', 'REPLACE', len(fields), fields)
+    user_sql = mysql.create_insert_sql('es_table', 'REPLACE', 9)
 
     df = df.reset_index()
     for index, row in df.iterrows():
         publish_date = datetime.strptime(row['publish_date'], '%Y-%m-%d')
-        news = [row['id'], row['title'], row['newspaper'], row['authors'], publish_date, row['keywords'], row['summary'], row['text'], row['url']]
+
+        dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        news = [row['id'], row['title'], row['newspaper'], row['authors'],
+                publish_date, row['keywords'], row['summary'], row['text'], row['url']]
         mysql.execute(user_sql, news)
         print(index, "record inserted")
