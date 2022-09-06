e5902f229903507e382594c2db091db8b5c73ecf
phuongtrannam <trannamphuonga197@gmail.com>
2022-09-06 23:11:04 +0900

    building crawler for cnn website

---
+++ crawler/cnn_crawler.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+#!/home/namphuong/Code/news-search-engine/myvenv/bin/python
+import sys
+
+
+class CNNCrawler:
+
+    def __init__(self,selected_date):
+        self.selected_date = selected_date
+
+    def get_link_articles(self):
+        url_list = []
+        selected_year = self.selected_date.split('-')[0]
+        selected_month = self.selected_date.split('-')[1]
+        selected_day = self.selected_date.split('-')[2]
+        targeted_url = self.generate_targeted_url(selected_year, selected_month)
+        html = requests.get(targeted_url)
+        html.encoding = 'utf-8'
+        sp = BeautifulSoup(html.text, 'lxml')
+        article_list = sp.findAll('div', {'class': 'sitemap-entry'})[1]
+
+        for article_element in article_list.find_all('li'):
+            article = article_element.find_all('span')
+            publish_date = article[0].get_text().split('-')[2]
+            
+            if (publish_date == selected_day):
+                link = article[1].find('a').get('href')
+                url_list.append(link)
+
+        return url_list
+
+    @staticmethod
+    def generate_targeted_url(year, month):
+        return "https://edition.cnn.com/article/sitemap-{}-{}.html".format(year, month)
+
+
+if __name__ == '__main__':
+    selected_date = sys.argv[1]
+    crawler = CNNCrawler(selected_date)
+    url_list = crawler.get_link_articles()
+    print(len(url_list))
