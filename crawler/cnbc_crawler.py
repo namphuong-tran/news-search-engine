485c8f54c0f8bd1d2e5a8cf5e50d1172bced440e
phuongtrannam <trannamphuonga197@gmail.com>
2022-09-06 23:52:15 +0900

    build cnbc crawler

---
+++ crawler/cnbc_crawler.py
@@ -0,0 +1,39 @@
+#!/home/namphuong/Code/news-search-engine/myvenv/bin/python
+import sys
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+
+class CNBCCrawler:
+
+    def __init__(self, selected_date):
+        self.selected_date = selected_date
+
+    def get_link_articles(self):
+        url_list = []
+        targeted_url = self.generate_targeted_url(self.selected_date)
+        html = requests.get(targeted_url)
+        html.encoding = 'utf-8'
+        sp = BeautifulSoup(html.text, 'lxml')
+        for article_element in sp.find_all('a', {'class': 'SiteMapArticleList-link'}):
+            url_list.append(article_element.get('href'))
+
+        return url_list
+
+    @staticmethod
+    def generate_targeted_url(selected_date):
+        year = selected_date.split('-')[0]
+        month_num = selected_date.split('-')[1]
+        # Mapping month number to full month name (Eg: 09 -> September)
+        month_name = datetime.datetime(1, int(month_num), 1).strftime("%B")
+        # Convert number in string to short style (Eg: 01 to 1)
+        day = int(selected_date.split('-')[2])
+        return "https://www.cnbc.com/site-map/articles/{}/{}/{}/".format(year, month_name, day)
+
+
+if __name__ == '__main__':
+    selected_date = sys.argv[1]
+    crawler = CNBCCrawler(selected_date)
+    url_list = crawler.get_link_articles()
+    print(len(url_list))
