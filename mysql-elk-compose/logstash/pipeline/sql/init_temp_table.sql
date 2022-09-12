6b8650b2973b5efafdab5538f84428ade239d51f
namphuongtran <trannamphuonga197@gmail.com>
2022-09-12 23:37:37 +0900

    refactor code

--- mysql-elk-compose/logstash/pipeline/sql/init_temp_table.sql
+++ mysql-elk-compose/logstash/pipeline/sql/init_temp_table.sql
@@ -1,6 +1,2 @@
--- SELECT * FROM temp_db.temp_table
--- ORDER BY id
-
-
 SELECT * FROM temp_db.es_table
 ORDER BY publish_date
--- mysql-elk-compose/logstash/pipeline/sql/sync_temp_table.sql
+++ mysql-elk-compose/logstash/pipeline/sql/sync_temp_table.sql
@@ -1,8 +1,3 @@
--- SELECT *, update_time AS ts FROM temp_db.temp_table
--- WHERE update_time > :sql_last_value AND update_time < NOW()
--- ORDER BY id
-
-
 SELECT *, modification_time AS ts FROM temp_db.es_table
 WHERE modification_time > :sql_last_value AND modification_time < NOW()
 ORDER BY publish_date
