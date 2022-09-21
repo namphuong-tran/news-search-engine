ec475ad59ab2050b042679cc67aa3366200e741b
namphuongtran <trannamphuonga197@gmail.com>
2022-09-22 01:28:45 +0900

    done redirect to search page and show result

--- web/api/article.py
+++ web/api/article.py
@@ -1,12 +1,12 @@
 from crypt import methods
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, session
 from crawler.newschannels import NewsChannels
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SelectMultipleField, SubmitField
 from wtforms.validators import DataRequired
 from wtforms.widgets import ListWidget, CheckboxInput
 from datetime import date
-import web.api.db as db 
+import web.api.db as db
 from flask_paginate import Pagination, get_page_parameter
 
 app = Flask(
@@ -34,6 +34,10 @@ class SearchForm(FlaskForm):
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    session['terms'] = None
+    session['startdate'] = None
+    session['enddate'] = None
+    session['selected_channels'] = None
     terms = None
     startdate = None
     enddate = None
@@ -42,27 +46,34 @@ def index():
     pagination = None
     form = SearchForm()
     if form.validate_on_submit():
-        terms = form.terms.data
-        startdate = form.startdate.data
-        enddate = form.enddate.data
-        selected_channels = form.selected_channels.data
-        print(type(selected_channels))
-        print(selected_channels)
-        # resp = db.search()
-        # total = resp.get('total')
-        # articles = resp.get('result')
+        # terms = form.terms.data
+        # startdate = form.startdate.data
+        # enddate = form.enddate.data
+        # selected_channels = form.selected_channels.data
+        # print(type(selected_channels))
+        # print(selected_channels)
+        # # resp = db.search()
+        # # total = resp.get('total')
+        # # articles = resp.get('result')
 
-        search = False
-        q = request.args.get('q')
-        if q:
-            search = True
-        
-        # search = True
-        total = 100
-        articles = [1,2,3,4,5,6,7,8,9,10]
+        # search = False
+        # q = request.args.get('q')
+        # if q:
+        #     search = True
 
-        page = request.args.get(get_page_parameter(), type=int, default=1)
-        pagination = Pagination(page=page, total=total, search=search, record_name='articles', css_framework='bootstrap5', show_single_page=True, per_page=3)
+        # # search = True
+        # total = 100
+        # articles = [1,2,3,4,5,6,7,8,9,10]
+
+        # page = request.args.get(get_page_parameter(), type=int, default=1)
+        # pagination = Pagination(page=page, total=total, search=search, record_name='articles', css_framework='bootstrap5', show_single_page=True, per_page=3)
+        # return render_template("index.html", terms=terms, startdate=startdate, enddate=enddate, selected_channels=selected_channels, form=form, articles=articles,
+        #                    pagination=pagination)
+        session['terms'] = form.terms.data
+        session['startdate'] = form.startdate.data.strftime('%Y-%m-%d')
+        session['enddate'] = form.enddate.data.strftime('%Y-%m-%d')
+        session['selected_channels'] = form.selected_channels.data
+        return redirect(url_for('search'))
 
     print(request.args.get(get_page_parameter(), type=int, default=1))
     return render_template("index.html", terms=terms, startdate=startdate, enddate=enddate, selected_channels=selected_channels, form=form, articles=articles,
@@ -71,35 +82,25 @@ def index():
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
-    # pagination = {"from": 0, "size": 10}
-    # matching_terms = "2021 Australian grand pix"
-    # date_range = {"start_date" : "2022-09-05", "end_date" : "2022-09-15"}
-    # news_channel = ["BBC", "CNN"]
-    # client = PythonClient()
-    # resp = client.search_articles(
-    #     pagination, matching_terms, date_range, news_channel)
-    # print("Got %d Hits:" % resp['hits']['total']['value'])
-    # for hit in resp['hits']['hits']:
-    #     print("%(publish_date)s %(title)s: \n %(summary)s" %
-    #           hit["_source"])
-    # # return "<p>Hello, World!</p>"
-    # # articles = resp['hits']['hits']
-    # articles = [article['_source'] for article in resp['hits']['hits']]
-    # # return [article.to_json() for article in articles]
-    # return {
-    #     "result": articles,
-    #     "from": pagination.get("from"),
-    #     "size": pagination.get("size"),
-    # }
+    terms = session['terms']
+    # startdate = session['startdate']
+    # enddate = session['enddate']
+    date_range = {
+        "start_date": session['startdate'], "end_date": session['enddate']}
+    selected_channels = session['selected_channels']
+    size = 2
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    print(page)
 
-    print(type(request.form.get("from")))
-    print(request.form.get("size"))
-    print(request.form.get("terms"))
-    print(request.form.get("start_date"))
-    print(request.form.get("end_date"))
-    print(type(request.form.get("news_channel")))
+    pagination = {"from": (page - 1) * size, "size": size}
+    resp = db.search(pagination, terms, date_range, selected_channels)
+    total = resp.get("total")
+    articles = resp.get("result") 
 
-    return "<p>Hello, World!</p>"
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    pagination = Pagination(page=page, total=total, record_name='articles',
+                            css_framework='bootstrap5', show_single_page=True, per_page=size)
+    return render_template("search.html", articles=articles, pagination=pagination)
 
 # Custom Error Pages
 
